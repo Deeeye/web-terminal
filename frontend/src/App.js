adb2b56fd93f124a3986b4ed61735536bc3425e4
devops-bot official <160845182+Deeeye@users.noreply.github.com>
2024-10-09 22:59:44 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import './App.css';
+import TerminalProvider from './context/TerminalContext';
+import TerminalManager from './components/TerminalManager';
+
+function App() {
+  return (
+    <TerminalProvider>
+      <div className="App">
+        <h1>Web Terminal Application</h1>
+        <TerminalManager />
+      </div>
+    </TerminalProvider>
+  );
+}
+
+export default App;
 
