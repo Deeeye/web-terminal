db4304af902b9961a62f4baa9f8ac0e255ef8572
devops-bot official <160845182+Deeeye@users.noreply.github.com>
2024-10-09 22:56:17 +0200

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,1 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  port: process.env.PORT || 3001,
+  ssh: {
+    host: process.env.SSH_HOST || 'localhost',
+    port: process.env.SSH_PORT || 22,
+    username: process.env.SSH_USERNAME || 'user',
+    password: process.env.SSH_PASSWORD || 'pass',
+  },
+};
 
